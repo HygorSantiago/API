2295e7af3e0d7731822568ac9ea9a972fd1e5a59
Hygor Santiago <89393953+HygorSantiago@users.noreply.github.com>
2025-05-26 18:04:55 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Print(Resource):
+    def get(self):
+        return {'Deu certo':'Deu certo'},200
+        
+api.add_resource(Print,
+                 '/print')
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+    
+    
